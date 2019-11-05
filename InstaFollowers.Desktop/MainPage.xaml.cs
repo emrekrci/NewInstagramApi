6b888d5aacb2bd0c98c7cc7e729660c060866475
emrekrci <kkirci.emre@gmail.com>
2019-11-05 22:52:48 +0300

    Add project files.

---
+++ InstaFollowers.Desktop/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace InstaFollowers.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public static string accesTokenUri = "https://api.instagram.com/oauth/access_token";
+        public static string accessToken = "";
+        public static string userID = "";
+        public MainPage()
+        {
+            InitializeComponent();
+            getAccessToken();
+        }
+
+        public async void getAccessToken()
+        {
+            var valueCollection = new Dictionary<string, string>
+            {
+                {"app_id", WebPage.appID },
+                { "app_secret", WebPage.appSecret},
+                { "grant_type", "authorization_code"},
+                {"redirect_uri", "https://github.com/emrekrci/" },
+                { "code", WebPage.code},
+            };
+
+            HttpClient client = new HttpClient();
+            var values = new FormUrlEncodedContent(valueCollection);
+            var response = await client.PostAsync("https://api.instagram.com/oauth/access_token", values);
+            var content = await response.Content.ReadAsStringAsync();
+            var result2 = JsonConvert.DeserializeObject<JObject>(content);
+            accessToken = result2.GetValue("access_token").ToString();
+            userID = result2.GetValue("user_id").ToString();
+            txtAccessToken.Text = accessToken;
+            txtUserID.Text = userID;
+        }
+    }
+}
---
+++ InstaFollowers.Desktop/WebPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InstaFollowers.Desktop
+{
+    /// <summary>
+    /// Interaction logic for WebPage.xaml
+    /// </summary>
+    public partial class WebPage : Page
+    {
+
+        public static string appID = "532747647568845";
+        public static string appSecret = "ec9f0ff3fc124fb48c8a96dbeb300706";
+        public static string redirectUri = HttpUtility.UrlEncode("https://github.com/emrekrci/");
+        public static string code = "";
+        public WebPage()
+        {
+            InitializeComponent();
+            OpenMainWeb();
+        }
+
+        private void OpenMainWeb()
+        {
+            webSiteUrl.Text = $"https://api.instagram.com/oauth/authorize?app_id={appID}&redirect_uri={redirectUri}&scope=user_profile,user_media&response_type=code";
+            webPanel.Navigate($"https://api.instagram.com/oauth/authorize?app_id={appID}&redirect_uri={redirectUri}&scope=user_profile,user_media&response_type=code");
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = webPanel.Source;
+            webSiteUrl.Text = uri.ToString();
+            var list = HttpUtility.ParseQueryString(uri.Query);
+            code = list.Get("code");
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            string url = webSiteUrl.Text;
+            webPanel.Navigate("https://" + url);
+        }
+    }
+}
